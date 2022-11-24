02006128dd91072bd49d7beb7048d01c22a6a8fe
Harrison Borges <hborges005@gmail.com>
2022-11-24 18:33:34 -0500

    render list of habits

--- lib/pages/habits_page.dart
+++ lib/pages/habits_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HabitsPage extends StatelessWidget {
+import '../constants.dart';
+
+class HabitsPage extends StatefulWidget {
   const HabitsPage({Key? key}) : super(key: key);
 
   @override
+  State<HabitsPage> createState() => _HabitsPageState();
+}
+
+class _HabitsPageState extends State<HabitsPage> {
+  List _habits = [];
+
+  Future<void> _getHabits() async {
+    // final userId = supabase.auth.currentUser!.id;
+
+    final data = await supabase.from('habits').select('*');
+
+    setState(() {
+      _habits = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getHabits();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Habits Page')),
+    return Scaffold(
+      body: Column(
+          children: _habits.isNotEmpty
+              ? _habits.map((habit) => Text(habit['name'])).toList()
+              : [const Text('Add Habits')]),
     );
   }
 }
